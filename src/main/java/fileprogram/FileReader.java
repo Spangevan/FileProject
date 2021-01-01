74166ce1fa11f3153bb1226a27396bad6eaedd19
Evan Spangenberg <ewlson23@gmail.com>
2021-01-01 17:21:33 -0500

    added to FileReader

--- src/main/java/fileprogram/FileReader.java
+++ src/main/java/fileprogram/FileReader.java
@@ -1,5 +1,19 @@
 package fileprogram;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileReader {
+	
+	public static void readFile() throws IOException {
+		File potatoLog = new File("/Users/evanspangenberg/FileProject/potatolog.txt");
+		FileInputStream stream = new FileInputStream(potatoLog);
+		int r = 0;
+		while((r = stream.read()) != -1) {
+			System.out.print((char)r);
+		}
+		
+	}
 
 }
